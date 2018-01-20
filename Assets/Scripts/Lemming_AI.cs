aab8e39718c6d93db7e7972859fa5c3c46862faa
Daniel Thomas Horga <thomas.horga@gmail.com>
2018-01-20 13:15:46 -0500

    update AI

--- Assets/Scripts/Lemming_AI.cs
+++ Assets/Scripts/Lemming_AI.cs
@@ -1,16 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
+using RootMotion.FinalIK;
 public class Lemming_AI : MonoBehaviour {
+
+	public GameObject target;
+
+	public NavMeshAgent agent;
+
+	private Animator animator;
+
+	public bool enableRagDoll;
+	private RagdollUtility ragdoll;
 
 	// Use this for initialization
 	void Start () {
-		
+		agent  = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
+		ragdoll = GetComponent<RagdollUtility>();
+		//agent.destination = target.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(enableRagDoll){
+			ragdoll.EnableRagdoll();
+			animator.SetBool("Walk",false);
+			animator.SetBool("Ragdoll",true);
+		}
+		else{
+			
+			ragdoll.DisableRagdoll();
+					//agent.Move();
+			if(Vector3.Distance(transform.position,target.transform.position) < 1){
+			animator.SetBool("Walk",false);
+			agent.isStopped = true;
+			
+			}
+			else{
+				//animator.SetBool("Walk",true);
+				agent.isStopped = false;
+			}
+			agent.destination = target.transform.position;
+		}
 		
+
+	}
+
+	public void Standup () {
+		print("animation event");
+		animator.SetBool("Walk",true);
+    	animator.SetBool("Ragdoll",false);
 	}
 }
