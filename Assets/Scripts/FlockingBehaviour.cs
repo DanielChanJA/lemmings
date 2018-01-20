1d8780eacc89c21871679cfd71573a3e6457bc65
Daniel Chan <iam@danielchan.me>
2018-01-20 14:27:25 -0500

    Flocking Mechanism

---
+++ Assets/Scripts/FlockingBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingBehaviour : MonoBehaviour {
+
+	[Header("Flock Settings")]
+	public int flockSize;
+	public Vector3 spawnLocation;
+	public int spawnRadius;
+	public GameObject lemming;
+
+	public List<GameObject> lemmings;
+	
+	
+	// Use this for initialization
+	void Start () {
+		lemmings = new List<GameObject>();
+
+		for(int i = 0; i < flockSize; i++) {
+			GameObject tempLemming = GameObject.Instantiate(lemming);
+			tempLemming.transform.position = new Vector3(
+				Random.Range(spawnLocation.x - spawnRadius, spawnLocation.x + spawnRadius), 
+				spawnLocation.y, 
+				Random.Range(spawnLocation.z - spawnRadius, spawnLocation.z + spawnRadius));
+			lemmings.Add(tempLemming);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
