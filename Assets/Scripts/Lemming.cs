2912f15dc309fb64e94395083d8162fbeb316137
IVeselovskyy <IVeselovskyy@users.noreply.github.com>
2018-01-23 21:17:09 -0500

    Fixed movement direction, added scrolling

--- Assets/Scripts/Lemming.cs
+++ Assets/Scripts/Lemming.cs
@@ -26,7 +26,7 @@ public class Lemming : MonoBehaviour {
  	public float topSpeed = 10;
 
 	// Use this for initialization
-	void Start () {
+	void Start(){
 		LemmingCollider = GetComponent<MeshCollider>();
 		LemmingRB = GetComponent<Rigidbody>();
          theCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
@@ -34,65 +34,61 @@ public class Lemming : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update(){
 		
 	}
 
  
-	void OnMouseDown()
-	{
-		if(mode == modes.Catapult) {
-			screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-		 
-			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	void OnMouseDown(){
 
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        if (mode == modes.Catapult){
+			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 			mMouseDownPos = Input.mousePosition;
-			Debug.Log( "the mouse down pos is " + mMouseDownPos);
-		 	
-			 dragging = true;
-			 //GetComponent<Rigidbody>().isKinematic = true;
-			 //GetComponent<Collider>().enabled = false;
-		} else 
-		{
-			 //This grabs the position of the object in the world and turns it into the position on the screen
-			 gameObjectSreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-			 //Sets the mouse pointers vector3
-			 mousePreviousLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
-		}
+		    dragging = true;
+            mousePreviousLocation = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+        else{
+            gameObjectSreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            //Sets the mouse pointers vector3
+            mousePreviousLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
+        }
 	}
  
-	void OnMouseDrag()
-	{
-		if(mode == modes.Catapult)
-			{
-			Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		 
-			Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-			transform.position = curPosition;
-	 	} else 
-		{
-			mouseCurLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
-			force = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
-			mousePreviousLocation = mouseCurLocation;
-			LemmingRB.velocity += (force * 0.2f);
-		}
-	}
+	void OnMouseDrag(){
+        Vector3 curScreenPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        if (mode == modes.Catapult){
+            force = curScreenPoint - mousePreviousLocation;
+            mousePreviousLocation = curScreenPoint;
+            LemmingRB.velocity += force;
+        }
+        else{
+            mouseCurLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
+            force = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
+            mousePreviousLocation = mouseCurLocation;
+            LemmingRB.velocity += (force * 0.2f);
+        }
+        var d = Input.GetAxis("Mouse ScrollWheel");
 
-	void OnMouseUp()
-	{
-		if(mode == modes.Catapult)
-		{
+        if (d > 0f){
+            transform.position = transform.position + (Camera.main.transform.forward * d * Time.deltaTime * 100f);
+        }
+        else if (d < 0f){
+            transform.position = transform.position + (Camera.main.transform.forward * d * Time.deltaTime * 100f);
+        }
+    }
+
+	void OnMouseUp(){
+		if(mode == modes.Catapult){
 			dragging = false;
-			GetComponent<Rigidbody>().isKinematic = false;
-			GetComponent<Collider>().enabled = true;
 			Vector3 left = theCam.transform.right *Input.GetAxis("Mouse X") * 10f;
 			Vector3 right = theCam.transform.up *Input.GetAxis("Mouse Y") * 10f;
 			Debug.Log(left);
 			Debug.Log(right);
 			GetComponent<Rigidbody>().AddForce(left, ForceMode.Impulse);
 			GetComponent<Rigidbody>().AddForce(right, ForceMode.Impulse);
-		} else 
-		{
+		} else{
 			if (LemmingRB.velocity.magnitude > topSpeed)
 		     		force = LemmingRB.velocity.normalized * topSpeed;
 		}
